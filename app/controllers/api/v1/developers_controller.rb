d6ee18ecf6a99276781a2c6449f9520a5b79f3b2
RodrighoNS <rnavarro@lemontech.com>
2021-11-08 13:09:40 -0300

    Add Api versioning

---
+++ app/controllers/api/v1/developers_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    class DevelopersController < ApplicationController
+      # GET /developers
+      def index
+        @developers = Developer.all
+        json_response(@developers)
+      end
+
+      # POST /developers
+      # create! raises an exception if there
+      # are errors in model validations
+      def create
+        @developer = Developer.create!(developer_params)
+        json_response(@developer, :created)
+      end
+
+      private
+
+      def developer_params
+        params.permit(:name, :role)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :developers
+  namespace :api do
+    namespace :v1 do
+      resources :developers
+    end
+  end
 end
