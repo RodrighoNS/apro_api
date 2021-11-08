6e47d7f012754d630709738d20330259e5b832ae
RodrighoNS <rnavarro@lemontech.com>
2021-11-08 13:23:35 -0300

    Update test with api versioning

--- spec/requests/developers_spec.rb
+++ spec/requests/developers_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe "Developers API", type: :request do
   let!(:developers) { create_list(:developer, 10) }
 
-  # Test suite for GET /Developers
+  # Test suite for GET /api/v1/Developers
   describe "GET /developers" do
-    before { get '/developers' }
+    before { get '/api/v1/developers' }
 
     it 'returns developers' do
       expect(json).not_to be_empty
@@ -17,12 +17,12 @@ RSpec.describe "Developers API", type: :request do
     end
   end
 
-  # Test suite for POST /Developers
-  describe 'POST /developers' do
+  # Test suite for POST /api/v1/Developers
+  describe 'POST /api/v1/developers' do
     let(:valid_attributes) { { name: 'Josefina', role: 'frontend dev' } }
 
     context 'when the request is valid' do
-      before { post '/developers', params: valid_attributes }
+      before { post '/api/v1/developers', params: valid_attributes }
 
       it 'creates a developer' do
         expect(json['name']).to eq('Josefina')
@@ -34,7 +34,7 @@ RSpec.describe "Developers API", type: :request do
     end
 
     context 'when the request is invalid' do
-      before { post '/developers', params: { name: 'Homero' } }
+      before { post '/api/v1/developers', params: { name: 'Homero' } }
 
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
